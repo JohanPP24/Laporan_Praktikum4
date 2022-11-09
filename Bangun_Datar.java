11923b9004a64c379ac72e968835ee6be1e52e68
JohanPP24 <92928548+JohanPP24@users.noreply.github.com>
2022-11-09 15:49:06 +0700

    Menambahkan file Bangun_Datar.java

---
+++ Bangun_Datar.java
@@ -0,0 +1,10 @@
+public class Bangun_Datar{
+    float luas(){
+        System.out.println(x= "Tampilkan Luas");
+        return 0;
+    }
+    float keliling(){
+        System.out.println(x= "Tampilkan Keliling");
+        return 0;
+    }
+}
